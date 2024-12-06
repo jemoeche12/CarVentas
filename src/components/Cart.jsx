86cb3c76c10e29cdc5b8e655bf25b35a63025c6b
jeronimo <jemoeche@gmail.com>
2024-12-06 13:18:51 -0300

    solucionandoAlert

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
-import MessageAlert from "./MessageAlert";
+import MessageAlert from './MessageAlert';
 import MessageDeleteCarrito from "./messageDeleteCarrito";
 
 const Cart = () => {
